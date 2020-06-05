d9002cad2bc2428e1317ee7c71e7f67e76bb31f9
Krishna Mohan <krishnamohan@authenticid.co>
2020-06-05 16:52:56 +0530

    added framework

---
+++ DemoFramework/DemoFramework/ModelClass.m
@@ -0,0 +1,15 @@
+//
+//  ModelClass.m
+//  DemoFramework
+//
+//  Created by AID011 on 05/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import "ModelClass.h"
+
+@implementation ModelClass
++(void)testingData{
+    NSLog(@"Krishna Mohan Ankeshwar");
+}
+@end
